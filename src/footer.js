2a8691335229161bd4dd80f306fa3da2a0ba89bd
Jason Soprovich <190052169+jasonsoprovich@users.noreply.github.com>
2025-04-24 20:59:48 -0600

    feat: add logo to footer

--- src/footer.js
+++ src/footer.js
@@ -1,7 +1,14 @@
+import createLogo from "./logo";
+
 function loadFooter() {
   const footerContainer = document.getElementById('footer-container');
 
   footerContainer.innerHTML = '';
+
+  const footerLogo = createLogo();
+  footerLogo.classList.add('footer-logo');
+
+  footerContainer.appendChild(footerLogo);
 
   const footerContent = document.createElement('p');
   footerContent.innerHTML = `
@@ -12,4 +19,19 @@ function loadFooter() {
 }
 
 export default loadFooter;
+
+// function loadFooter() {
+//   const footerContainer = document.getElementById('footer-container');
+
+//   footerContainer.innerHTML = '';
+
+//   const footerContent = document.createElement('p');
+//   footerContent.innerHTML = `
+//      2025 Shio. All rights reserved.
+//   `;
+
+//   footerContainer.appendChild(footerContent);
+// }
+
+// export default loadFooter;
 
